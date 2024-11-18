3b6e18511e6405d69b4eac2821dffcf09bcbfd6b
mmandyck88 <mealva20@asu.edu>
2024-11-18 06:31:00 -1000

    Update hw_funcprog1_template.rkt

--- hw_funcprog1_template.rkt
+++ hw_funcprog1_template.rkt
@@ -2,8 +2,6 @@
 ; CSE240 Functional Programming 1 Homework
 ; This assignment involves pracicing various expressions and procedures in Scheme.
 ; 
-; For the first assignment, we are providing the default header so you can see how it
-; looks in Scheme. For future assignments, you will need to add it yourself.
 ;
 ; Completion time: 8 hours
 ;
